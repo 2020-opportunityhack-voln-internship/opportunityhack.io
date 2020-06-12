a30cd09d6b54a4d5671d9312d5e16ec2e079eb77
Brian Bruns <bbruns97@users.noreply.github.com>
2020-06-12 16:01:06 -0700

    finished top banner

--- oppio/src/components/homepage_banner.js
+++ oppio/src/components/homepage_banner.js
@@ -1,10 +1,13 @@
 import React from "react"
+import BannerImage from "../images/banner_img.jpg"
+import LogoImageOver from "../images/Logos/White/square_w.png"
 
 class Banner extends React.Component {
     render(){
         return (
             <div id='homepage_banner'>
-                
+                <img id='banner_img_under' src={BannerImage}></img>
+                <img id='banner_img_over' src={LogoImageOver}></img>
             </div>
         )
     }
