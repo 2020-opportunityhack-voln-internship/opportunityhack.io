61728adf292253a06283a432ffea0e8aebd24498
Brian Bruns <bbruns97@users.noreply.github.com>
2020-06-12 18:44:23 -0700

    Banners responsive. Advanced Scrolling

--- oppio/src/components/homepage_banner.js
+++ oppio/src/components/homepage_banner.js
@@ -3,11 +3,17 @@ import BannerImage from "../images/banner_img.jpg"
 import LogoImageOver from "../images/Logos/White/square_w.png"
 
 class Banner extends React.Component {
+    scrollToTools(){
+        document.querySelector('#banner_1').scrollIntoView({ block: 'start',  behaviour: 'smooth' });
+    }
     render(){
         return (
             <div id='homepage_banner'>
                 <img id='banner_img_under' src={BannerImage}></img>
-                <img id='banner_img_over' src={LogoImageOver}></img>
+                <div id='inner_banner'>
+                    <img id='banner_img_over' src={LogoImageOver}></img>
+                    <p id='banner_scroll_btn' onClick={this.scrollToTools}>See our tools</p>
+                </div>
             </div>
         )
     }
--- oppio/src/pages/index.js
+++ oppio/src/pages/index.js
@@ -19,6 +19,16 @@ const HomePage = () => (
   </Helmet>
   <TopNav />
   <Banner />
+  <div id='banner_1' class='info_banner'>
+    <h1 class='banner_heading'>What is OpportunityHack?</h1>
+   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br></br>
+   <a class='banner_link' href='/'>Learn more</a>
+   </p>
+  </div>
+  <div id='banner_2' class='info_banner'>
+   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+  </div>
+  
   </div>
   
 )
