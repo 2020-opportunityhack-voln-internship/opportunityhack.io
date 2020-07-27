710a55bec0ff60520f01ae04afa40a34bdd7b68d
abc <sandya7c@gmail.com>
2020-07-27 14:20:39 -0700

    Adds slideshow component in research labs

--- oppio/src/pages/research.js
+++ oppio/src/pages/research.js
@@ -3,20 +3,34 @@ import { Helmet } from "react-helmet"
 import { graphql } from "gatsby"
 import "../components/layout.css"
 import TopNav from "../components/topnav"
+import B1 from "../../static/banner_4.jpg";
+import B2 from "../../static/banner_5.jpg";
+import B3 from "../../static/banner_6.jpg";
+import B4 from "../../static/banner_7.jpg";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
-export default ({ data }) => {
+export default function App({data}) {
+  const renderSlides = () =>
+    [B1,B2,B3,B4].map(num => (
+      <div>
+	 <img class='banner_img_under' src={num} alt='home page banner'></img>
+      </div>
+    ));
 
-return (
-  <body>
-    
-    {/* HELMET */}
+  return (
+<React.Fragment>
+    <div className="App">
+      <Slider dots={true}>{renderSlides()}</Slider>
+    </div>
+
     <Helmet>
       <title>OpportunityHack</title>
       <script src="https://apis.google.com/js/platform.js" async defer></script>
       <meta name="google-signin-client_id" content="275198785754-2qtchf3m7l14iper2iorstghppp4rv8l.apps.googleusercontent.com"/>
     
     </Helmet>
-    {/* END OF HELMET */}
 
     {/* NAV */}
     <TopNav />
@@ -31,7 +45,7 @@ return (
   <div class='small_border gray'></div>
 
   <div id='research_wrap'>
-  
+
   {/* Iterate through all markdown files and generate solution thumbnails. */}
   {data.allMarkdownRemark.edges.map(({ node }) => (
     <div class='research'>
@@ -42,6 +56,7 @@ return (
       <p class='soln_desc'>{node.frontmatter.mini_description}</p>
 
       </div>
+
   {/* End of research */}
 
     </div>
@@ -53,11 +68,11 @@ return (
   <br></br>
 
   </div>
-
-  </body>
   
-)
+   </React.Fragment>
+  );
 }
+
 
 export const query = graphql`
   query {
