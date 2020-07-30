bfb2a4172f9dc2e65082d5b07742e10e529bddaa
Brian Bruns <bbruns97@users.noreply.github.com>
2020-07-30 03:03:57 -0700

    CSS fixes. CMS config update. Awaiting approval from org

--- oppio/src/templates/solutionDetails.js
+++ oppio/src/templates/solutionDetails.js
@@ -32,7 +32,7 @@ export default ({ data }) => {
               <div dangerouslySetInnerHTML={{ __html: post.html }} />
               </p>
               <center>
-                <a id='banner_link' href="/#" onClick={() => 
+                <a id='banner_link' onClick={() => 
                   window.scroll({top: document.querySelector('#banner_3').offsetTop - 50})}>View tutorial</a>
               </center>
               <br></br><br></br>
